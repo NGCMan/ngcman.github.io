025770de502d4362298943856bff5eaed2782ff2
NGCMan <nintendogamecubeman@gmail.com>
2019-10-14 18:01:31 -0700

    JS Coordinates

--- Text Based RPG/script.js
+++ Text Based RPG/script.js
@@ -1,5 +1,36 @@
 var coordx = 0
 var coordy = 0
+var dungeonLevel = 0
         window.onload = function(){
+document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeonLevel + " of the dungeon.";
+          
+document.getElementById("coordinates").innerHTML = "You are located at: (" + coordx + "," + coordy + ")";
+}
+
+function North() {
+  coordy++
+  document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeonLevel + " of the dungeon.";
+          
+document.getElementById("coordinates").innerHTML = "You are located at: (" + coordx + "," + coordy + ")";
+}
+
+function South() {
+  coordy--
+  document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeonLevel + " of the dungeon.";
+          
+document.getElementById("coordinates").innerHTML = "You are located at: (" + coordx + "," + coordy + ")";
+}
+
+function West() {
+  coordx--
+  document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeonLevel + " of the dungeon.";
+          
+document.getElementById("coordinates").innerHTML = "You are located at: (" + coordx + "," + coordy + ")";
+}
+
+function East() {
+  coordx++
+  document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeonLevel + " of the dungeon.";
+          
 document.getElementById("coordinates").innerHTML = "You are located at: (" + coordx + "," + coordy + ")";
 }
