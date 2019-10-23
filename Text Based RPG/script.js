39c60caebe7cb534b13c89a80fd8f67e218393b6
NGCMan <nintendogamecubeman@gmail.com>
2019-10-23 10:01:01 -0700

    Update script.js

--- Text Based RPG/script.js
+++ Text Based RPG/script.js
@@ -1,10 +1,25 @@
 var coordx = 0
 var coordy = 0
 var dungeonLevel = 0
+var encounter = 0
+var movement = document.getElementById("movement");
+var attack = document.getElementById("attack");
+var TheWholeGame = document.getElementById("TheWholeGame");
+// RPG Stats
+var hp = 24
+var mp = 12
+var atk = 2
+var def = 1
+
+var enemyHp = 6
+var enemyMp = 0
+var enemyAtk = 3
+var enemyDef = 0
         window.onload = function(){
 document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeonLevel + " of the dungeon.";
 
 document.getElementById("coordinates").innerHTML = "You are located at: (" + coordx + "," + coordy + ")";
+  attack.style.display = "none";
 }
 
 function North() {
@@ -18,6 +33,32 @@ function North() {
     document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeonLevel + " of the dungeon.";
 
 document.getElementById("coordinates").innerHTML = "You are located at: (" + coordx + "," + coordy + ")";
+  if (dungeonLevel==1) {
+    // Thanks to Mozilla for the function!
+    function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+ encounter = (getRandomInt(10) + 1);
+    
+    if (encounter==10) {
+        document.getElementById("surroundings").innerHTML = "A bug appears!"
+enemyHp = 6
+enemyMp = 0
+enemyAtk = 3
+enemyDef = 0
+      movement.style.display = "none";
+      attack.style.display = "block";
+      var y = document.getElementsByClassName("enemy");
+  y[2].innerHTML = "HP: " + enemyHp;
+var y = document.getElementsByClassName("enemy");
+  y[4].innerHTML = "MP: " + enemyMp;
+var y = document.getElementsByClassName("enemy");
+  y[6].innerHTML = "ATK: " + enemyAtk;
+var y = document.getElementsByClassName("enemy");
+  y[8].innerHTML = "DEF: " + enemyDef;
+    }
+  }
 }
 
 
@@ -77,6 +118,10 @@ document.getElementById("surroundings").innerHTML = "There is a staircase leadin
   }
 }
  }
+  if (hp <= 0) {
+  TheWholeGame.style.display = "none";
+  document.getElementById("GameOver").innerHTML = "<h1>Game Over!</h1>"
+}
 }, 50);
 
 function CDown() {
@@ -101,4 +146,64 @@ document.getElementById("dungeonLevel").innerHTML = "You are at Floor " + dungeo
   }
 }
  }
+}
+
+var x = document.getElementsByClassName("players");
+  x[2].innerHTML = "HP: " + hp;
+var x = document.getElementsByClassName("players");
+  x[4].innerHTML = "MP: " + mp;
+var x = document.getElementsByClassName("players");
+  x[6].innerHTML = "ATK: " + atk;
+var x = document.getElementsByClassName("players");
+  x[8].innerHTML = "DEF: " + def;
+
+function Attack() {
+  //Not the best formula, but it works.
+  enemyHp -= atk+enemyDef
+  hp -= enemyAtk+def
+  var x = document.getElementsByClassName("players");
+  x[2].innerHTML = "HP: " + hp;
+var x = document.getElementsByClassName("players");
+  x[4].innerHTML = "MP: " + mp;
+var x = document.getElementsByClassName("players");
+  x[6].innerHTML = "ATK: " + atk;
+var x = document.getElementsByClassName("players");
+  x[8].innerHTML = "DEF: " + def;
+        var y = document.getElementsByClassName("enemy");
+  y[2].innerHTML = "HP: " + enemyHp;
+var y = document.getElementsByClassName("enemy");
+  y[4].innerHTML = "MP: " + enemyMp;
+var y = document.getElementsByClassName("enemy");
+  y[6].innerHTML = "ATK: " + enemyAtk;
+var y = document.getElementsByClassName("enemy");
+  y[8].innerHTML = "DEF: " + enemyDef;
+  if (enemyHp <= 0) {
+      encounter = 0
+          attack.style.display = "none";
+      movement.style.display = "block";
+      }
+}
+function Defend() {
+  def *= 1.5
+  hp -= enemyAtk+def
+  def /= 1.5
+    var x = document.getElementsByClassName("players");
+  x[2].innerHTML = "HP: " + hp;
+var x = document.getElementsByClassName("players");
+  x[4].innerHTML = "MP: " + mp;
+var x = document.getElementsByClassName("players");
+  x[6].innerHTML = "ATK: " + atk;
+var x = document.getElementsByClassName("players");
+  x[8].innerHTML = "DEF: " + def;
+        var y = document.getElementsByClassName("enemy");
+  y[2].innerHTML = "HP: " + enemyHp;
+var y = document.getElementsByClassName("enemy");
+  y[4].innerHTML = "MP: " + enemyMp;
+var y = document.getElementsByClassName("enemy");
+  y[6].innerHTML = "ATK: " + enemyAtk;
+var y = document.getElementsByClassName("enemy");
+  y[8].innerHTML = "DEF: " + enemyDef;
+}
+function Magic() {
+  //To be announced...
 }
